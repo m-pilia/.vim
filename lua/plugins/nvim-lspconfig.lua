8b68fd8b6396ad16edc1a644a7a93dcbc434c817
Martino Pilia <martino.pilia@gmail.com>
2025-02-03 13:26:04 +0100

    Simplify lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,13 +2,15 @@ return {
     'neovim/nvim-lspconfig',
 
     config = function()
-        require('lspconfig').bashls.setup({})
-        require('lspconfig').clangd.setup({})
-        require('lspconfig').jsonls.setup({})
-        require('lspconfig').lua_ls.setup(require('config.lsp.lua_ls'))
-        require('lspconfig').pyright.setup({})
-        require('lspconfig').qmlls.setup({})
-        require('lspconfig').vimls.setup({})
+        local lspconfig = require('lspconfig')
+
+        lspconfig.bashls.setup({})
+        lspconfig.clangd.setup({})
+        lspconfig.jsonls.setup({})
+        lspconfig.lua_ls.setup(require('config.lsp.lua_ls'))
+        lspconfig.pyright.setup({})
+        lspconfig.qmlls.setup({})
+        lspconfig.vimls.setup({})
 
         vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
             vim.lsp.diagnostic.on_publish_diagnostics,
