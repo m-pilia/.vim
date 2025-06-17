19a95c8712e4404fd556efcd687a776404f5a453
Martino Pilia <martino.pilia@gmail.com>
2025-06-17 13:26:47 +0200

    Add keymap to toggle lsp_signature window

--- lua/plugins/lsp_signature.nvim.lua
+++ lua/plugins/lsp_signature.nvim.lua
@@ -3,5 +3,9 @@ return {
 
     config = function()
         require('lsp_signature').setup({})
+
+        vim.keymap.set('i', '<C-s>', function()
+            require('lsp_signature').toggle_float_win()
+        end, {silent = true, noremap = true})
     end
 }
