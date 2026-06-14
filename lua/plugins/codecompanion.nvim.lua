cd782f23fbf07cd1622a63d0afc8237925adde48
Martino Pilia <martino.pilia@proton.me>
2026-06-14 08:04:33 +0200

    Update CodeCompanion adapter config

--- lua/plugins/codecompanion.nvim.lua
+++ lua/plugins/codecompanion.nvim.lua
@@ -1,22 +1,24 @@
 local function get_adapter()
-    if vim.env.GEMINI_API_KEY then
+    if vim.env.NVIM_CODECOMPANION_STRATEGY == 'mistral' then
         return {
             chat = {
-                adapter = 'gemini',
+                adapter = 'mistral_vibe',
             },
             inline = {
-                adapter = 'gemini',
+                adapter = 'mistral_vibe',
+            },
+        }
+    elseif vim.env.NVIM_CODECOMPANION_STRATEGY == 'copilot' then
+        return {
+            chat = {
+                adapter = 'copilot',
+            },
+            inline = {
+                adapter = 'copilot',
             },
         }
     else
-        return {
-            chat = {
-                adapter = 'copilot',
-            },
-            inline = {
-                adapter = 'copilot',
-            },
-        }
+        return {}
     end
 end
 
@@ -30,8 +32,8 @@ return {
 
     config = function()
         require('codecompanion').setup({
-            ignore_warnings = true,
-            strategies = get_adapter(),
+            ignore_warnings = false,
+            interactions = get_adapter(),
         })
     end
 }
